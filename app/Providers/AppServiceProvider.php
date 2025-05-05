7f62ca26a2a5a497e6421db998ed580003f5a11b
Ibinsss <ibin.shrs@gmail.com>
2025-05-05 10:08:01 +0800

    Remove final Telescope references

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,9 +11,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        if ($this->app->environment('local', 'staging')) {
-            $this->app->register(\App\Providers\TelescopeServiceProvider::class);
-        }
+      
     }
 
     /**
